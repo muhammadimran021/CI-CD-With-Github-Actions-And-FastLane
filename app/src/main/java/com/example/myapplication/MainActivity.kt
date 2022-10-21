46bf7c497cc90adf01a470c215a5d095e61cf24a
muhammad imran <muhammad_imran01@hotmail.com>
2022-10-22 00:22:44 +0500

    this is some new changes and please github create apk for firebase ;)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        Log.d("TAG", "onCreate: ")
     }
 }
