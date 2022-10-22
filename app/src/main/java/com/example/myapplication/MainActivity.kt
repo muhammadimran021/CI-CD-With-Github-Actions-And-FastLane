1aac80ca955021cd2fc9807c9a55bc2c8af885e1
muhammad imran <muhammad_imran01@hotmail.com>
2022-10-23 00:53:57 +0500

    check jobs and branch name

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("TAG", "create firebase app: ")
+        BuildConfig.DEBUG
     }
 }
